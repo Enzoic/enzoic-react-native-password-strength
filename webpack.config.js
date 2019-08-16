41418afc35ee6fa6817543679851808947ada8eb
Alex Cook <alex@alex-cook-mbp.local>
2019-08-16 13:11:06 -0600

    Setup webpack for bundling. Exclude react/react-dom/react-native

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  entry: "./src/index.js",
+  externals: {
+    "react-native": "react-native",
+    react: "react",
+    "react-dom": "react-dom"
+  },
+  output: {
+    filename: "../dist/index.js",
+    library: "EnzoicReactNativePasswordMeter",
+    libraryTarget: "umd",
+    path: __dirname
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: /src/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env', 
+              "@babel/preset-react", 
+              {'plugins': ['@babel/plugin-proposal-class-properties']}
+            ]
+          },
+        },
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: "url-loader"
+      }
+    ]
+  }
+};
